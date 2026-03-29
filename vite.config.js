880047b7774a8cb8248337eb4b087f2dd43a22dd
Ayushkumar12 <ayushkumarak112003@gmail.com>
2026-03-29 09:48:09 +0530

    Revert "olio"
    This reverts commit c32becbc8c9c3c6a1d62b54510d7de4531adf7be.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,14 +9,5 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
-
-
-
